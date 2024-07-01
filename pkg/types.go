0cb2e4ee4b2cd2e9fdf642e4efed138bb3dff546
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-07-01 23:29:41 +0200

    initial `C++` support

--- pkg/types.go
+++ pkg/types.go
@@ -1,12 +1,26 @@
 package pkg
 
 type JSON struct {
-	Compiler string  `json:"compiler"`
-	FileName string  `json:"file_name"`
-	Include  Include `json:"include"`
+	Schema               string               `json:"$schema"`
+	Language             string               `json:"language"`
+	Compiler             string               `json:"compiler"`
+	FileName             string               `json:"file_name"`
+	Include              Include              `json:"include"`
+	AdditionalCPPOptions AdditionalCPPOptions `json:"additional_cpp_options"`
 }
 
 type Include struct {
-	C []string
-	H []string
+	C   []string `json:"C"`
+	CPP []string `json:"C++"`
+	H   []string `json:"H"`
+	HPP []string `json:"H++"`
+}
+
+type AdditionalCPPOptions struct {
+	Extensions Extensions `json:"extensions"`
+}
+
+type Extensions struct {
+	Code   string `json:"code"`
+	Header string `json:"header"`
 }
