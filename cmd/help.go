46972ac49c74be04c1000a02ae31b593f3671795
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-07-02 22:31:17 +0200

    c++ support

--- cmd/help.go
+++ cmd/help.go
@@ -1,18 +1,15 @@
 package cmd
 
-import "fmt"
+import "github.com/pterm/pterm"
 
 func Help() {
-	fmt.Println("List of all commands:")
-	fmt.Print("\n")
-	fmt.Println("---------------------------------------------------------------------------")
-	fmt.Println("|'cpkgs add <package-name>' - add C packages using cpkgs                  |")
-	fmt.Println("|'cpkgs help' - shows this menu                                           |")
-	fmt.Println("|'cpkgs init <dir-name> [-d]' - initialize a new project using cpkgs      |")
-	fmt.Println("|'cpkgs install' - install all of the packages in the current project     |")
-	fmt.Println("|'cpkgs remove' - removes project directory                               |")
-	fmt.Println("|'cpkgs run <file-name>' - run the file name using your selected compiler |")
-	fmt.Println("|'cpkgs uninstall <package-name>' - removes the provided C package        |")
-	fmt.Println("|'cpkgs update <package-name> [-a]' - updates the provided C package      |")
-	fmt.Println("---------------------------------------------------------------------------")
+	cmd1 := "'cpkgs add <package-name>' - add C packages using cpkgs\n"
+	cmd2 := "'cpkgs help' - shows this menu\n"
+	cmd3 := "'cpkgs init <dir-name> [-d]' - initialize a new project using cpkgs\n"
+	cmd4 := "'cpkgs install' - install all of the packages in the current project\n"
+	cmd5 := "'cpkgs remove' - removes project directory\n"
+	cmd6 := "'cpkgs run <file-name>' - run the file name using your selected compiler\n"
+	cmd7 := "'cpkgs uninstall <package-name>' - removes the provided C package\n"
+	cmd8 := "'cpkgs update <package-name> [-a]' - updates the provided C package\n"
+	pterm.DefaultBox.WithTitle("List of all commands").WithTitleTopCenter().Print(cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7, cmd8)
 }
--- pkg/types.go
+++ pkg/types.go
@@ -1,12 +1,22 @@
 package pkg
 
 type JSON struct {
-	Compiler string  `json:"compiler"`
-	FileName string  `json:"file_name"`
-	Include  Include `json:"include"`
+	Schema        string         `json:"$schema"`
+	Language      string         `json:"language"`
+	Compiler      string         `json:"compiler"`
+	FileName      string         `json:"file_name"`
+	Include       Include        `json:"include"`
+	CPPExtensions *CPPExtensions `json:"cpp_extensions,omitempty"`
 }
 
 type Include struct {
-	C []string
-	H []string
+	C   []string `json:"C"`
+	CPP []string `json:"C++"`
+	H   []string `json:"H"`
+	HPP []string `json:"H++"`
+}
+
+type CPPExtensions struct {
+	Code   string `json:"code,omitempty"`
+	Header string `json:"header,omitempty"`
 }
