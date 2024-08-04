99da11b4f980bd8a22a992d8d9b652d58fb964e7
LoryPelli <lorypelli@proton.me>
2024-08-04 18:34:26 +0200

    Message if dir does not exists

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,10 +1,12 @@
 import { existsSync } from 'fs';
 import { file } from './utils/constants.js';
-import { info, success } from './utils/logs.js';
+import { info, success, warning } from './utils/logs.js';
 import remove from './utils/remove.js';
 
 if (existsSync(file)) {
     info(`Removing ${file}...`);
     await remove(file);
     success('File successfully removed!');
+} else {
+    warning('Directory does not exists, nothing was changed!');
 }
