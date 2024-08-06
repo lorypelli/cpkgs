a7f54355a85e6bc23babb14bfd9b53f9491c4af6
LoryPelli <lorypelli@proton.me>
2024-08-06 12:46:35 +0200

    fix run command

--- cmd/run.go
+++ cmd/run.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"flag"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -50,17 +51,19 @@ func Run() {
 		}
 	}
 	cmd := pterm.Sprintf("cd %s && %s -o cpkgs/bin/%s %s %s", path, JSON.Compiler, fname, f, strings.Join(flag.Args()[c:], " "))
-	files, err := os.ReadDir(pterm.Sprintf("%s/cpkgs", path))
-	if err != nil {
+	if err := filepath.WalkDir(pterm.Sprintf("%s/cpkgs", path), func(p string, f fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !f.IsDir() && strings.HasSuffix(f.Name(), ".c") {
+			cmd += pterm.Sprintf(" %s", strings.TrimPrefix(p, pterm.Sprintf("%s/", path)))
+		}
+		return nil
+	}); err != nil {
 		pterm.Error.Println(err)
 		return
 	}
-	for _, f := range files {
-		if strings.HasSuffix(f.Name(), ".c") {
-			cmd += pterm.Sprintf(" cpkgs/%s", f.Name())
-		}
-	}
-	cmd += pterm.Sprintf(" && cd cpkgs/bin && %s", JSON.FileName)
+	cmd += pterm.Sprintf(" && ./cpkgs/bin/%s", JSON.FileName)
 	cmdExec := exec.Command("sh", "-c", cmd)
 	if runtime.GOOS == "windows" {
 		cmdExec = exec.Command("cmd", "/C", cmd)
